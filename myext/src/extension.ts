cccf1695d1f7e8ea61aa70f1d990fa79d3036cc3
John Cortell <j.cortell@sarc.samsung.com>
2023-08-11 12:37:45 -0500

    Add a context menu to the webview
    Add the Hello World command to it, that shows an informational
    message in the app. Add a new command to open the webview.

--- myext/src/extension.ts
+++ myext/src/extension.ts
@@ -20,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('myext.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('myext.webview', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 
@@ -76,6 +76,11 @@ export function activate(context: vscode.ExtensionContext) {
 		);
 	});
 
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('myext.helloWorld', () => {
+		vscode.window.showInformationMessage("Hello World from VS Code extension");
+	});
 	context.subscriptions.push(disposable);
 }
 
