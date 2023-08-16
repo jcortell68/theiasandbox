a111ab6c6dd34a38cf585653f1bd594fb310d80f
John Cortell <j.cortell@sarc.samsung.com>
2023-08-16 13:09:01 -0500

    Add workaround to views_welcome bug
    Theia has a bug wherby the view_welcome contributions isn't
    used if the view isn't given a tree data provider. Give it
    one that returns empty results.
    
    Also, add an activation event for the view so that the extension
    is activated when Theia needs to show the view.

--- myext/src/extension.ts
+++ myext/src/extension.ts
@@ -27,13 +27,12 @@ export class MyTreeDataProvider implements vscode.TreeDataProvider<String> {
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Trigger the bug. The welcome view we declared in package.json should
+	// The welcome view we declared in package.json should
 	// fill the view if the view has no content (which will be the case if we
 	// don't register a tree data provider). That works in VS Code but not in
 	// Theia. In Theia, you have to at least register a provider. If it
-	// returns an empty collection, then the welcome view is used. So, to work
-	// around the Theia bug and see the welecome voew, uncomment out this line.
-	// vscode.window.registerTreeDataProvider("myextview", new MyTreeDataProvider());
+	// returns an empty collection
+	vscode.window.registerTreeDataProvider("myextview", new MyTreeDataProvider());
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
