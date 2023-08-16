0b08358e235da408ead0fab3b07152a939001189
John Cortell <j.cortell@sarc.samsung.com>
2023-08-16 13:03:38 -0500

    Add an activation event for the view.
    Also, code the workaround but disable it, so that the bug is
    reproduced by default.

--- myext/src/extension.ts
+++ myext/src/extension.ts
@@ -2,21 +2,38 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const supressContent = true;
+
 // Simple tree data provider for our view. Just returns three items.
 export class MyTreeDataProvider implements vscode.TreeDataProvider<String> {
 	getTreeItem(element: String): vscode.TreeItem | Thenable<vscode.TreeItem> {
-		return new vscode.TreeItem("" + element);
+		if (supressContent) {
+			return {};
+		}
+		else {
+			return new vscode.TreeItem("" + element);
+		}
 	}
 	getChildren(element?: String | undefined): vscode.ProviderResult<String[]> {
-		return Promise.resolve(["this", "that", "whatever"]);
+		if (supressContent) {
+			return new Promise((resolve) => resolve([]));
+		}
+		else {
+			return Promise.resolve(["this", "that", "whatever"]);
+		}
 	}
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Disabling this so our viewsWelcome is shown...
-	// vscode.window.registerTreeDataProvider("myext.view", new MyTreeDataProvider());
+	// Trigger the bug. The welcome view we declared in package.json should
+	// fill the view if the view has no content (which will be the case if we
+	// don't register a tree data provider). That works in VS Code but not in
+	// Theia. In Theia, you have to at least register a provider. If it
+	// returns an empty collection, then the welcome view is used. So, to work
+	// around the Theia bug and see the welecome voew, uncomment out this line.
+	// vscode.window.registerTreeDataProvider("myextview", new MyTreeDataProvider());
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
